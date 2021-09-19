63fc172b6021fc144947412120686eb4ea3aecad
husseinalkarmi <husseinalkarmi012@gmail.com>
2021-09-19 17:22:43 +0300

    lab02

--- my-app/src/components/Hornedbeast.js
+++ my-app/src/components/Hornedbeast.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+
 class Hornedbeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Clicks: 0,
+    };
+  }
+
+  increaseClicks = () => {
+    this.setState({
+      Clicks: this.state.Clicks + 1,
+    });
+  };
+
   render() {
     return (
       <div>
-        <h2>{this.props.title} </h2>
-        <img
-          src={this.props.img}
-          alt={this.props.keyword}
-          title={this.props.horns}
-        />
-
-        <p>{this.props.description}</p>
+        <Col>
+          <Card className="card">
+            <Card.Img className="cardi" variant="top" src={this.props.img} />
+            <Card.Body>
+              <Card.Title>{this.props.title}</Card.Title>
+              <Card.Text> number of clicks {this.state.Clicks}</Card.Text>
+              <Button onClick={this.increaseClicks} variant="primary">clicks</Button>
+            </Card.Body>
+          </Card>
+        </Col>
       </div>
     );
   }
 }
-
 export default Hornedbeast;
